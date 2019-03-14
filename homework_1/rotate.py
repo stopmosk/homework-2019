91751a382c2e05623f9992d13eff606036538b5a
Sergei Shutov <48527749+stopmosk@users.noreply.github.com>
2019-03-14 22:08:26 +0300

    Переделал структуру папок

---
+++ homework_1/rotate.py
@@ -0,0 +1,6 @@
+def rotate(lst):
+    result = [lst[-1]] + lst[0:-1]
+    return result
+
+# ls = [1, 2, 3, 4]
+# print(rotate(ls))
--- rotate.py
+++
@@ -1,6 +0,0 @@
-def rotate(lst):
-    result = [lst[-1]] + lst[0:-1]
-    return result
-
-# ls = [1, 2, 3, 4]
-# print(rotate(ls))
