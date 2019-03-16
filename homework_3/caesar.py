5b1ea35aa71629b5f8a3e7a936e24fd9ee04224a
Sergei Shutov <48527749+stopmosk@users.noreply.github.com>
2019-03-16 15:23:00 +0300

    Homework 3 upd

--- homework_3/caesar.py
+++ homework_3/caesar.py
@@ -4,9 +4,11 @@ print('Добрейший вечерочик. Будем щифровать (e) 
 s = input('Введите команду: ')
 text = input('Введите текст: ')
 offset = int(input('Введите смещение: '))
+if not -25 <= offset <= 25:
+    raise ValueError('Смещение должно находиться в рамках [-25..25].')
 
 if s == 'e':
     print(encrypt(offset, text))
-if s == 'd:':
+if s == 'd':
     print(decrypt(offset, text))
 
--- homework_3/caesar_logic.py
+++ homework_3/caesar_logic.py
@@ -1,35 +1,25 @@
-def check_args(offset, text):
+def crypt(offset, text):
     if not isinstance(offset, int) or not isinstance(text, str):
         raise TypeError('Неправильный тип аргументов. Нужны int и str.')
-    if offset < 0 or offset > 25:
-        raise ValueError('Смещение должно находиться в рамках [0..25].')
-
-
-def crypt(offset, text):
-    check_args(offset, text)
+    offset %= 26
     result_text = ''
     for symbol in text:
-        if symbol == ' ':
-            result_text += ' '
+        ord_s = ord(symbol)
+        if ord_s < 65 or 90 < ord_s < 97 or ord_s > 122:
+            result_text += symbol
         else:
-            ord_s = ord(symbol)
-            if 65 <= ord_s <= 90 < ord_s + offset or 97 <= ord_s <= 122 < ord_s + offset:
-                offset -= 26
-            if ord_s < 65 or 90 < ord_s < 97 or ord_s > 122:
-                raise ValueError('Строка на входе должна состоять только из символов a..z и A..Z и пробелов')
-            result_text += chr(ord_s + offset)
+            if 65 <= ord_s <= 90 < ord_s + offset or \
+                    97 <= ord_s <= 122 < ord_s + offset:
+                result_text += chr(ord_s + offset - 26)
+            else:
+                result_text += chr(ord_s + offset)
     return result_text
 
 
 def encrypt(offset, text):
-    check_args(offset, text)
     return crypt(offset, text)
 
 
 def decrypt(offset, text):
-    check_args(offset, text)
     return crypt(26 - offset, text)
 
-
-# print(encrypt(1, 'aAbBcCxXyYzZ'))
-# print(decrypt(1, 'aAbBcCxXyYzZ'))
