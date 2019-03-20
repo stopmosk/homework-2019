c7fbea2f0f3adaa95b9e6a0e923163e064a8a338
Sergei Shutov <48527749+stopmosk@users.noreply.github.com>
2019-03-20 16:30:23 +0300

    Апдейт ДЗ-4

--- hw4/task3/trades.py
+++ hw4/task3/trades.py
@@ -6,11 +6,17 @@ print('')
 print('******** ОКНО С МАКСИМАЛЬНЫМ КОЛИЧЕСТВОМ СДЕЛОК **********')
 print('  Окно             Кол-во сделок        Сумма сделок')
 max_gap, max_qty, max_sum = max_qty_gap(db)
-print(max_gap, max_qty, max_sum, sep='\t\t\t\t')
+print(max_gap, max_qty, int(max_sum), sep='\t\t\t\t')
+print('**********************************************************')
+print('***************** В ТОМ ЧИСЛЕ ПО БИРЖАМ ******************')
+# print(db[max_gap])
+mg_db = db[max_gap]
+# print(mg_db['J'])
+for ex in mg_db:
+    print(ex, '\t\t', mg_db[ex][0], '\t\t\t', int(mg_db[ex][1]))
+print()
 print('**********************************************************')
 print()
-# db_max = db[max_gap]
-# print(db_max)
 
 print('****** ОКНА С МАКСИМАЛЬНЫМ КОЛИЧЕСТВОМ СДЕЛОК ПО БИРЖАМ ******')
 for ex in ex_list:
@@ -18,5 +24,5 @@ for ex in ex_list:
     print(ex)
     max_gap, max_qty, max_sum = max_qty_gap_ex(db, ex)
     print('  Окно             Кол-во сделок        Сумма сделок')
-    print(max_gap, max_qty, max_sum, sep='\t\t\t\t')
+    print(max_gap, max_qty, int(max_sum), sep='\t\t\t\t')
     print('******************************************************')
