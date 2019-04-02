c0f1666751a74e9bbb8e468773a438e99967742b
Sergei Shutov <48527749+stopmosk@users.noreply.github.com>
2019-04-02 14:21:47 +0300

    Update Klava101.py

--- hw5/task1/Klava101.py
+++ hw5/task1/Klava101.py
@@ -1,30 +1,30 @@
 import random
 
 
+def show_bug(chance):
+    rnd = random.randrange(1, 100)
+    if 1 <= rnd <= chance:
+        return True
+
+
 sim_len = 1000   # int(input('Введите длину симуляции:'))
-chance = 11
+bug_chance = 11
 test_run = 17
 testing_time = 1.5 * 60
 
 random.seed()
 
+result = ''
 for k in range(sim_len):
+    print(result, end='')
     result = 'F'
-    for i in range(int(testing_time // test_run)):
-        s = random.randrange(1, 100)
-        if 1 <= s <= chance:
-            result = 'L'
-            print(result, end='')
-            break
 
+    for i in range(int(testing_time // test_run)):
+        if show_bug(bug_chance):
+            result = 'L'
+            break
     if result == 'L':
         continue
-
-    for i in range(int(testing_time // test_run)):
-        s = random.randrange(1, 100)
-        if 1 <= s <= chance:
-            result = 'S'
-            print(result, end='')
-            break
-
-    print(result, end='')
+    if show_bug(bug_chance):
+        result = 'S'
+        continue
